2ae2d4ab6dac062c4af4142174e521199b3b233b
Johannes Horner <lzy.info@pm.me>
2023-06-24 15:50:42 +0200

    check if node under cursor is a todo_item

--- lua/neorg/modules/core/timetrack/module.lua
+++ lua/neorg/modules/core/timetrack/module.lua
@@ -15,6 +15,7 @@ module.setup = function()
     return {
         requires = {
             "core.tempus",
+            "core.qol.todo_items",
         },
     }
 end
@@ -25,17 +26,31 @@ module.load = function()
     end)
 end
 
-local function clock_in()
-    local tempus = module.required["core.tempus"]
-    local current_time = tostring(tempus.to_date(os.date("*t"), true))
-    vim.api.nvim_put({ "{@ " .. current_time .. "}" }, "c", false, true)
+local function clock_in(event)
+    -- local ts_utils = module.required["core.integrations.treesitter"].get_ts_utils()
+    local mod_todo = module.required["core.qol.todo_items"]
+
+    -- cursor not on a todo item
+    local node = mod_todo.get_todo_item_from_cursor(event.buffer, event.cursor_position[1] - 1)
+    if not node then
+        neorg.utils.notify("cursor not on a todo item", vim.log.levels.ERROR)
+        return
+    else
+    end
+
+    -- local current_node = ts_utils.get_node_at_cursor()
+    -- local found_node = module.required["core.integrations.treesitter"].find_parent(current_node, {})
+
+    -- local tempus = module.required["core.tempus"]
+    -- local current_time = tostring(tempus.to_date(os.date("*t"), true))
+    -- vim.api.nvim_put({ "{@ " .. current_time .. "}" }, "c", false, true)
 end
 
 local function clock_out() end
 
 module.on_event = function(event)
     if event.split_type[2] == "core.timetrack.clock-in" then
-        clock_in()
+        clock_in(event)
     elseif event.split_type[2] == "core.timetrack.clock-out" then
         clock_out()
     end
