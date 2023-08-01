ee56e6430150b0a3a0a2543c621b3fd21959ae51
Johannes Horner <lzy.info@pm.me>
2023-08-01 19:38:41 +0200

    clocking in kinda working

--- lua/neorg/modules/core/timetrack/module.lua
+++ lua/neorg/modules/core/timetrack/module.lua
@@ -6,6 +6,7 @@
     ---
 --]]
 
+local neorg = require("neorg.core")
 local module = neorg.modules.create("core.timetrack")
 
 module.setup = function()
@@ -26,20 +27,37 @@ end
 
 local function clock_in(event)
     local mod_todo = module.required["core.qol.todo_items"]
+    local mod_ts = module.required["core.integrations.treesitter"]
 
-    local node = mod_todo.get_todo_item_from_cursor(event.buffer, event.cursor_position[1] - 1)
+    local todo_node = mod_todo.get_todo_item_from_cursor(event.buffer, event.cursor_position[1] - 1)
 
     -- cursor not on a todo item
-    if not node then
-        neorg.utils.notify("cursor not on a todo item", vim.log.levels.ERROR)
+    if not todo_node then
+        -- not an error, just for debugging purposes
+        neorg.utils.notify("not on a todo node", vim.log.levels.ERROR)
         return
     end
+
+    local todo_line_idx, _, _ = todo_node:start()
+    local next_node = mod_ts.get_first_node_on_line(event.buffer, todo_line_idx + 1)
+    if not next_node then
+        -- not an error, just for debugging purposes
+        neorg.utils.notify("next node nil", vim.log.levels.ERROR)
+        return
+    end
+
+    if next_node:type() ~= "ranged_verbatim_tag" then
+        local line_idx, _, _ = next_node:start()
+        vim.api.nvim_buf_set_lines(0, line_idx, line_idx, true, { "@timetrack", "@end" })
+        next_node = mod_ts.get_first_node_on_line(event.buffer, todo_line_idx + 1)
+    end
+
+    local tag_end_line_idx, _, _ = next_node:end_()
 
     local tempus = module.required["core.tempus"]
     local current_time = tostring(tempus.to_date(os.date("*t"), true))
 
-    local current_line_idx = vim.api.nvim_win_get_cursor(0)[1] - 1
-    vim.api.nvim_buf_set_lines(0, current_line_idx + 1, current_line_idx + 1, true, { "{@ " .. current_time .. "}" })
+    vim.api.nvim_buf_set_lines(0, tag_end_line_idx, tag_end_line_idx, true, { "{@ " .. current_time .. "}" })
 end
 --
 -- local function clock_in(event)
